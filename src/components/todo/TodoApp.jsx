29d2dc2f740847e79f16ba0e58d16544eb24e49d
qscd154 <qscd154@gmail.com>
2024-02-20 01:17:03 +0900

    url를 통해 웰컴 컴포넌트로 username파라미터 전달, todo 컴포넌트 추가

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -7,10 +7,11 @@ export default function TodoApp() {
         <div className="TodoApp">
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<LoginComponent/>}> </Route>
-                    <Route path='/login' element={<LoginComponent/>}> </Route>
-                    <Route path='/welcome' element={<WelcomeComponent/>}> </Route>
-                    <Route path='*' element={<ErrorComponent/>}> </Route>
+                    <Route path='/' element={<LoginComponent/>}/>
+                    <Route path='/login' element={<LoginComponent/>}/>
+                    <Route path='/welcome/:username' element={<WelcomeComponent/>}/>
+                    <Route path='/todos' element={<ListTodoComponent/>}/>
+                    <Route path='*' element={<ErrorComponent/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
@@ -37,7 +38,7 @@ function handleSubmit() {
     if(username==='in28minutes' && password === 'dummy') {
         setShowSuccessMessage(true)
         setShowErrorMessage(false)
-        navigate('/welcome')
+        navigate(`/welcome/${username}`)
     } else {
         setShowSuccessMessage(false)
         setShowErrorMessage(true)
@@ -88,11 +89,15 @@ function handleSubmit() {
 
 
 function WelcomeComponent() {
+
+    const {username} =  useParams()
+
+
     return (
         <div className="WelcomeComponent">
-            <h1>Welcome in28minutes</h1>
+            <h1>Welcome {username}</h1>
             <div>
-                Welcome Component
+                Manage Your todos - <Link to='/todos'>Go here</Link>
             </div>
         </div>
     )
@@ -104,6 +109,61 @@ function ErrorComponent() {
             <h1>We are working really hard!</h1>\
             <div>
                 Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+            </div>
+        </div>
+    )
+}
+
+function ListTodoComponent() {
+
+    const today = new Date();
+
+    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+
+    const todos = [
+        {id:1, description: 'Learn AWS', done:false, targetDate:targetDate},
+        {id:2, description: 'Learn Full Stack Dev', done:false, targetDate:targetDate},
+        {id:3, description: 'Learn DevOps', done:false, targetDate:targetDate},
+    
+    ]
+
+
+
+
+
+    return (
+        <div className="ListTodoComponent">
+            <h1>Things You Want To Do!</h1>
+            <div>
+
+                <table>
+
+                    <thead>
+                        <tr>
+                            <td>ID</td>
+                            <td>Description</td>
+                            <td>Is Done?</td>
+                            <td>Target Date</td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                    {
+                        todos.map(
+                            todo => (
+                                <tr key={todo.id}>
+                                    <td>{todo.id}</td>
+                                    <td>{todo.description}</td>
+                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.targetDate.toDateString()}</td>
+                                </tr>
+                            )
+                        )
+                    }
+                    </tbody>
+
+                </table>
+
             </div>
         </div>
     )
