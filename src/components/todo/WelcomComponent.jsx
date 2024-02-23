5582eb9704c0c3353b2809bdf445052ba882e803
qscd154 <qscd154@gmail.com>
2024-02-24 01:47:40 +0900

    Axios 리팩터링

--- src/components/todo/WelcomComponent.jsx
+++ src/components/todo/WelcomComponent.jsx
@@ -1,18 +1,36 @@
 import {useParams, Link} from 'react-router-dom'
-import axios from 'axios'
+import { useState } from 'react'
+import {  retrieveHelloWorldPathVariable } from './api/HelloWorldApiService'
 function WelcomeComponent() {
 
     const {username} =  useParams()
 
+    const [message, setMessage] = useState(null)
+
     function callHelloWorldRestApi() {
-        axios.get('http://localhost:8080/hello-world')
-        .then( (response) => successfulResponse(response) )
-        .catch( (error) => errorResponse(error))
-        .finally(() => console.log('cleanup'))
+
+    
+        // retrieveHelloWorldBean()
+        //     .then( (response) => successfulResponse(response) )
+        //     .catch( (error) => errorResponse(error))
+        //     .finally(() => console.log('cleanup'))
+
+        retrieveHelloWorldPathVariable('Ranga')
+            .then( (response) => successfulResponse(response) )
+            .catch( (error) => errorResponse(error))
+            .finally(() => console.log('cleanup'))
+
+        // axios.get('http://localhost:8080/hello-world')
+        // .then( (response) => successfulResponse(response) )
+        // .catch( (error) => errorResponse(error))
+        // .finally(() => console.log('cleanup'))
+
     }
 
     function successfulResponse(response) {
-        console.log(response)
+        console.log(response.data.message)
+        setMessage(response.data.message)
+
     }
 
     function errorResponse(error) {
@@ -29,6 +47,7 @@ function WelcomeComponent() {
                 <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>
                     Call Hello World</button>
             </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
